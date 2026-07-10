56cdc8afcab633bf8c3ff6f0b195bede5c83d0f5
Maurice Hildebrandt <hello@mauricehildebrandt.ch>
2026-07-10 16:29:59 +0200

    Fix entry listing not using inertia navigation

--- addons/morhi/statamic-inertia/stubs/entry-listing-examples/EntryPreviews/Default.vue
+++ addons/morhi/statamic-inertia/stubs/entry-listing-examples/EntryPreviews/Default.vue
@@ -1,11 +1,13 @@
 <template>
-  <a :href="entry.url" class="block-entry-listing-default group py-4 border-b border-white/[0.07] flex items-baseline justify-between gap-4">
+  <Link :href="entry.url" class="block-entry-listing-default group py-4 border-b border-white/[0.07] flex items-baseline justify-between gap-4">
     <h3 class="text-white font-medium group-hover:text-sky-400 transition-colors duration-200">{{ entry.title }}</h3>
     <time v-if="entry.date" :datetime="entry.date" class="text-gray-500 text-sm shrink-0">{{ entry.date }}</time>
-  </a>
+  </Link>
 </template>
 
 <script lang="ts" setup>
+import { Link } from '@inertiajs/vue3'
+
 defineOptions({ inheritAttrs: false })
 
 interface ListedEntry {
